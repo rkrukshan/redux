105a63857f82a48a0aba442199c59b2a9ab8156e
rkrukshan <rukshan1122@gmail.com>
2025-10-23 09:17:06 +0530

    project setup

---
+++ shopping-cart/src/components/Product.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Product() {
+    const [products, getProducts] = useState([]);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products").then(data => data.json()).then(result => getProducts(result))
+    }, []);
+    return (
+        <div>
+            <h1>Product Detail</h1>
+            {JSON.stringify(products)}
+        </div>
+    )
+}
